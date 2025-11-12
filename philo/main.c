2a604f55f871d5bfd86094c454427eb3c2f20e2e
suat kıvam <suatkivam@gmail.com>
2025-11-12 15:55:28 +0300

    Rename eating, sleeping, and thinking functions for consistency; add missing header file for philosopher data structures

--- philo/main.c
+++ philo/main.c
@@ -108,7 +108,7 @@ void	init_mutexes_and_philos(t_philo_data *philo_data,
 	}
 }
 
-void	eat(t_philo *philo)
+void	eat_routine(t_philo *philo)
 {
 	// Çatal ataması sayesinde (Adım 2), bu sıralama
 	// artık kilitlenmeye (deadlock) yol açmaz.
@@ -117,7 +117,7 @@ void	eat(t_philo *philo)
 	pthread_mutex_lock(philo->right_fork);
 	write_status(philo, "has taken a fork");
 	// yemek yeme
-	write_status(philo, "İs eating");
+	write_status(philo, "is eating");
 	pthread_mutex_lock(&philo->data->data_lock);
 	philo->last_eat_time = get_current_time();
 	philo->eat_count++;
@@ -127,15 +127,15 @@ void	eat(t_philo *philo)
 	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_unlock(philo->left_fork);
 }
-void	sleep(t_philo *philo)
+void	sleep_routine(t_philo *philo)
 {
-	write_status(philo, "Is sleeping");
+	write_status(philo, "is sleeping");
 	usleep(philo->data->time_to_sleep * 1000);
 }
 
-void	think(t_philo *philo)
+void	think_routine(t_philo *philo)
 {
-	write_status(philo, "Is thinking");
+	write_status(philo, "is thinking");
 }
 void	start_simulation(t_philo_data *philo_data)
 {
@@ -185,11 +185,18 @@ void	*philo_routine(void *argument)
 	// 3. Kapı açıldı! Kapıdan geç ve hemen serbest bırak ki
 	//    diğer filozoflar da geçebilsin.
 	pthread_mutex_unlock(&philo->data->start_gate_lock);
+	// 1 philo dururmu
+	if (philo->data->number_of_philosophers == 1)
+	{
+		write_status(philo, "has taken a fork");
+		usleep(philo->data->time_to_die * 1000 + 1000);
+		return (NULL);
+	}
 	while (philo->data->is_dead == 0)
 	{
-		eat(philo);
-		sleep(philo);
-		think(philo);
+		eat_routine(philo);
+		sleep_routine(philo);
+		think_routine(philo);
 		// kodla dedik marsa kaçıp intahar ettiler
 	}
 	return (NULL);
